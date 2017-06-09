fae9398c42f0cf32eb465fe2b327d2717bdcea16
Panarin Michael <michaelpanarin@inbox.ru>
2017-06-09 17:31:48 +0300

    added a simple view for further development

--- material_blog/core/views.py
+++ material_blog/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from posts.models import BlogPost, Category
+from users.models import User
+
+
+class HomeView(View):
+    def get(self, request):
+        posts = BlogPost.objects.filter(author_id=request.user.id)
+        amount = len(posts)
+        categories = Category.objects.all()
+        return render(request, 'home.html', {'posts': posts[:10], 'amount': amount, 'categories': categories})
--- material_blog/material_blog/urls.py
+++ material_blog/material_blog/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+from core.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^redactor/', include('redactor.urls')),
-]
+    url(r'^home/', HomeView.as_view(), name="home"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
